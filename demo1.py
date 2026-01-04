ada112492e7052cedd34eb7715629241cbcac3bf
Dennis <55298962+dennishore@users.noreply.github.com>
2026-01-04 14:14:20 -0800

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,8 @@
+from time import sleep_ms
+from machine import Pin 
+led=Pin(2,Pin.OUT) #create LED object from pin2,Set Pin2 to output 
+while True: 
+    led.value(1) #Set led turn on 
+    sleep_ms(500) 
+    led.value(0) #Set led turn off 
+    sleep_ms(100)
